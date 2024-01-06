329c16feeaec9a4fbd13930a96e06a82ff68b28d
Coteh <3276350+Coteh@users.noreply.github.com>
2024-01-06 01:46:01 -0500

    Add prettier

--- cli/app.js
+++ cli/app.js
@@ -10,10 +10,9 @@ const argv = require("yargs")
     .alias("km", "keep-marker")
     .alias("h", "help")
     .describe({
-        "verbose": "Print extra messages",
-        "keep-marker": "Keeps the APP1 marker in the JPEG"
-    })
-    .argv;
+        verbose: "Print extra messages",
+        "keep-marker": "Keeps the APP1 marker in the JPEG",
+    }).argv;
 
 var imageArr = argv._;
 
@@ -31,37 +30,36 @@ imageArr.forEach((elem) => {
 });
 
 var images = imageArr.map((elem) => {
-                try {
-                    var destFileName = elem.split(".");
-                    destFileName.splice(destFileName.length - 1, 0, "modified");
-                    return {
-                        buffer: fs.readFileSync(elem),
-                        srcFileName: elem,
-                        destFileName: destFileName.join(".")
-                    };
-                } catch (e) {
-                    if (e.code === "ENOENT") {
-                        console.log("Error opening '" + elem + "'. Image file does not exist.");
-                    } else {
-                        console.log("Unknown error opening '" + elem + "'. Error code: " + e.code);
-                    }
-                    process.exit(1);
-                }
-            });
+    try {
+        var destFileName = elem.split(".");
+        destFileName.splice(destFileName.length - 1, 0, "modified");
+        return {
+            buffer: fs.readFileSync(elem),
+            srcFileName: elem,
+            destFileName: destFileName.join("."),
+        };
+    } catch (e) {
+        if (e.code === "ENOENT") {
+            console.log("Error opening '" + elem + "'. Image file does not exist.");
+        } else {
+            console.log("Unknown error opening '" + elem + "'. Error code: " + e.code);
+        }
+        process.exit(1);
+    }
+});
 
 images.forEach((imageFileInfo) => {
     var imageFile = imageFileInfo.buffer;
 
     if (argv.verbose) {
         console.log("Processing '" + imageFileInfo.srcFileName + "'");
-        if (argv.verbose > 1)
-            console.log(imageFile);
+        if (argv.verbose > 1) console.log(imageFile);
         console.log("Image file length: " + imageFile.length);
     }
 
     var result = exifRemove.remove(imageFileInfo.buffer, {
         keepMarker: argv.keepMarker,
-        verbose: argv.verbose
+        verbose: argv.verbose,
     });
 
     if (argv.verbose)
@@ -73,5 +71,5 @@ images.forEach((imageFileInfo) => {
             return;
         }
         console.log("File written successfully.");
-    })
+    });
 });
